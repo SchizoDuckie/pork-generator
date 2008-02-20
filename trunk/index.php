193c6cf409f43dafb5748a7038bcbe8617576947
schizoduckie <schizoduckie@30e71338-8645-0410-b218-3b6fd03b77de>
2008-02-20 23:50:36 +0000

    moved some stuff here from mocha.js

--- trunk/index.php
+++ trunk/index.php
@@ -13,6 +13,7 @@ $_TPL['baseDir'] = 'http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER["SCRIPT_NAME
 $_TPL['title'] = 'P.O.R.K. - PHP On Rails (kinda)';
 $_TPL['scripts'][] = './includes/mootools.js';
 $_TPL['scripts'][] = './includes/mootools.ext.js';
+$_TPL['scripts'][] = './includes/excanvas.js';
 $_TPL['scripts'][] = './includes/pork.iframe.js';
 $_TPL['scripts'][] = './includes/porkfunctions.js';
 $_TPL['scripts'][] = './includes/mocha.js';
@@ -45,7 +46,29 @@ if(!dbConnection::getInstance()->connect())
 
 LoadPlugins('./plugins/');
 
-$_TPL['script'] .= "window.addEvent('load', function() {
+$_TPL['script'] .= "
+window.addEvent('load', function() {
+	
+	document.mochaScreens = new MochaScreens();
+	document.mochaDesktop = new MochaDesktop();
+	
+
+	$$('#mochaDesktopNavbar UL UL A').each(function(el){
+		alert('adding event to '+el)
+		el.addEvent('click', function(e) {
+			new Event(e).stop();
+			document.mochaDesktop.newWindow({
+					id: 'database_'+this.innerHTML,
+					title: 'Database: '+this.innerHTML,
+					loadMethod: 'xhr',
+					contentURL: this.href,
+					width: 320,
+					height: 320
+			});
+		});
+		
+	});	
+
 	var windowtje = document.mochaDesktop.newWindow({
 				id: 'welcome',
 				title: 'Welcome',
